a0248ef7ff98b885c182b5c367921077ebc66619
nicekoo <nicekoo@gmail.com>
2014-05-20 19:15:38 +0900

    DBConnector 분리

---
+++ MemberManagement/src/com/abc/member/DBConnectionManager.java
@@ -0,0 +1,26 @@
+package com.abc.member;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnectionManager {
+	private static DBConnectionManager dbConnManager; 
+	private static final String jdbcUrl = "jdbc:sqlite:test.db";
+	private static final String jdbcDriverName = "org.sqlite.JDBC";
+	
+	
+	private DBConnectionManager() throws ClassNotFoundException, SQLException {
+        //JDBC드라이버 로드
+        Class.forName(jdbcDriverName);
+	}
+
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated constructor stub
+		if(dbConnManager==null){
+			dbConnManager = new DBConnectionManager();
+		}
+        //DBMS에 연결하여 Connection 객체 얻기.
+		return DriverManager.getConnection(jdbcUrl);
+	}
+}
